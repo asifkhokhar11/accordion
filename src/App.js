fecc0cd8d0e4d93f91a650ddee7ea7798dae0630
asif khokhar <117714415+asifkhokhar11@users.noreply.github.com>
2024-01-28 07:57:11 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import Accordion from './component/Accordion';
+
+function App() {
+  return (
+    <>
+      <Accordion />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/api.jsx
@@ -0,0 +1,26 @@
+export const question = [
+    {
+        id: 1,
+        question: 'What is the virtual DOM?',
+        answer: 'DOM stands for document Object Model. The DOM represents an HTML document with a logical tree structure.'
+
+    },
+    {
+        id: 2,
+        question: 'How do you create a React app?',
+        answer: 'Install the create-react-app packge using the commmand prompt or terminal, '
+
+    },
+    {
+        id: 3,
+        question: 'What is an event i React',
+        answer: ' An event is an action that a user or system may trigger, such as pressing a key , a mouse click, etc. '
+
+    },
+    {
+        id: 4,
+        question: 'What are synthetic events in React?',
+        answer: "synthetic events combine the response of different browser's native events into one Api, ensuring that "
+
+    },
+]
